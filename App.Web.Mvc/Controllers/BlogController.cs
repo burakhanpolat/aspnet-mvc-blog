6c9b6731d9bb88c0abe44a01a0ec2255d848ea31
burakhanpolat <133880669+burakhanpolat@users.noreply.github.com>
2023-06-03 22:46:51 +0300

    Add files via upload

---
+++ App.Web.Mvc/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvc.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Search(string query, int page)
+        {
+            return View();
+        }
+        [Route("/blog/title-slug")]
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
